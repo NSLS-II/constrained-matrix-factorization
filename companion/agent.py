6c1a441a9ceb62631c9fcf32142791ec45cdd52b
maffettone <phillip.maffettone@gmail.com>
2020-12-22 07:27:36 -0500

    Initial plotting and companion DirectoryAgent

---
+++ companion/agent.py
@@ -0,0 +1,154 @@
+from pathlib import Path
+import numpy as np
+from time import sleep, time
+import matplotlib.pyplot as plt
+from companion.nmf import decomposition, example_plot
+from IPython import display
+
+
+class DirectoryAgent:
+
+    def __init__(self, data_dir, n_components, *, data_spec=None, x_lim=None, output_dir=None, header=0,
+                 file_ordering=None, file_limit=None, figsize=None, **kwargs):
+        """
+        Class for building trained model and classifying a directory.
+        Classification can be accomplished on the fly or once using the spin() method.
+
+        Parameters
+        ----------
+        data_dir: pathlike
+            Directory containing data to be classified
+        n_components:  int
+            Number of components for NMF
+        data_spec: basestring
+            String specification for glob() method in searching data_dir
+            Default behavior is to include all files. If writing temporary files, it is important to include
+            final file spec such as '*.xy'.
+        x_lim:  tuple
+            Size two tuple for bounding the Xs to a region of interest
+        header: int
+            Number of header lines in file
+        training_output_dir: pathlike
+            Output directory of training containing checkpoints for loading
+        path_to_model: pathlike
+            path to model to load in full (not presently implemented)
+        file_ordering: function
+            Function for sorting file paths as key argument in sorted
+        file_limit: int
+            Maximum number of files to consider
+        figsize: tuple
+            Two integer tuple for matplotlib figsize. Keep in mind all plots appear in a row.
+        **kwargs:
+            Keyword arguments to pass to companion.nmf.decomposition
+        """
+
+        self.dir = Path(data_dir).expanduser()
+        self.n_components = n_components
+        if data_spec is None:
+            self.path_spec = '*'
+        else:
+            self.path_spec = data_spec
+        self.output_dir = output_dir
+        self.paths = []
+        self.Ys = []
+        self.Xs = []
+        self.limit = file_limit
+        self.x_lim = x_lim
+        self.header = header
+        self.fig = plt.figure(figsize=figsize)
+        self.decomposition_args = kwargs
+
+        # TODO: Elegantly understand if I'm in a GUI or in ipython inline
+        try:
+            self.fig.canvas.manager.show()
+        except:
+            display.display(self.fig)
+
+        if file_ordering is None:
+            self.file_ordering = lambda x: x
+
+    def __len__(self):
+        return len(self.paths)
+
+    def path_list(self):
+        return list(self.dir.glob(self.path_spec))
+
+    def load_file(self, paths):
+        xs = []
+        ys = []
+        paths = sorted(paths)
+        for idx, path in enumerate(paths):
+            if not (self.limit is None) and idx >= self.limit:
+                break
+            _x, _y = np.loadtxt(path, comments='#', skiprows=self.header).T
+            xs.append(_x)
+            ys.append(_y)
+        return xs, ys
+
+    def update_plot(self):
+        if len(self) < 2:
+            return
+        idxs = [x for x, y in sorted(enumerate(self.paths), key=lambda x: self.file_ordering(x[1]))]
+        Xs = np.array(self.Xs)
+        Ys = np.array(self.Ys)
+        sub_X, sub_Y, alphas, components = decomposition(
+            Xs,
+            Ys,
+            q_range=self.x_lim,
+            n_components=self.n_components,
+            normalize=True,
+            **self.decomposition_args
+
+        )
+
+        axes = self.fig.subplots(1, self.n_components + 2)
+        example_plot(sub_X, sub_Y, alphas, axes=axes[:-1], sax=axes[-2],
+                     components=components, comax=axes[-1],
+                     alt_ordinate=np.array(idxs), summary_fig=True)
+
+        # TODO: Elegantly understand if I'm in a GUI or in ipython inline
+        self.fig.canvas.draw_idle()
+        self.fig.canvas.flush_events()
+        display.clear_output(wait=True)
+        display.display(self.fig)
+
+    def spin(self, sleep_delay=60, verbose=False, timeout=0):
+        """
+        Starts the spin to read new files and append their classifications to the output and internal dictionary
+        If a single pass of the data_directory is required, use a short or negative timeout time.
+        This can be run as a multiprocessing.Process target with a Manager to retain the output list if being run dynamically.
+
+        Parameters
+        ----------
+        sleep_delay: float
+            number of seconds to wait before checking directory for new files
+        verbose: bool
+            Print classifications to screen
+        timeout: float
+            Time to wait before stop spinning. Default is infinite spin. If a single pass is required, use a negative value.
+
+        Returns
+        -------
+        self.classifications:
+            dictionary of file basenames and classifications
+
+        """
+        start_time = time()
+
+        while True:
+            if len(self.path_list()) > len(self):
+                self.fig.clf()
+                for path in self.path_list():
+                    if path.name not in self.paths:
+                        self.paths.append(path.name)
+                        xs, ys = self.load_file([path])
+                        self.Xs.extend(xs)
+                        self.Ys.extend(ys)
+            self.update_plot()
+            if timeout and time() - start_time > timeout:
+                break
+            sleep(sleep_delay)
+        return np.array(self.Xs), np.array(self.Ys)
+
+# TODO: Implement RemoteDispatcher and Accumulator agents. See
+#  https://github.com/NSLS-II-XPD/ae-gpcam/blob/main/companion/agent.py
---
+++ companion/nmf.py
@@ -0,0 +1,217 @@
+"Non-negative matrix factorization of full datasets"
+from nmf.models import NMF, NMFD
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+
+
+def decomposition(Q,
+                  I,
+                  n_components=3,
+                  q_range=None,
+                  initial_components=None,
+                  fix_components=(),
+                  mode="Linear",
+                  kernel_width=1,
+                  max_iter=10000,
+                  bkg_removal=None,
+                  normalize=False,
+                  **kwargs):
+    """
+    Decompose and label a set of I(Q) data with optional focus bounds. Can be used for other
+    1-dimensional response functions, written with total scattering in mind.
+
+    Two operating modes are available: Linear (conventional) and Deconvolutional. The former will proceed as conventional
+    NMF as implemented in sklearn, with the added flexibility of the torch implementation. The latter will include a
+    convolutional kernel in the reconstruction between the component and weight matricies.
+
+    Initial components can be set as starting conditions of the component matrix for the optimization. These components
+    can be fixed or allowed to vary using the `fix_components` argument as a tuple of booleans.
+
+    Keyword arguments are passed to the fit method
+
+    Parameters
+    ----------
+    Q : array
+        Ordinate Q for I(Q). Assumed to be rank 2, shape (m_patterns, n_data)
+    I : array
+        The intensity values for each Q, assumed to be the same shape as Q. (m_patterns, n_data)
+    n_components: int
+        Number of components for NMF
+    q_range : tuple, list
+        (Min, Max) Q values for consideration in NMF. This enables a focused region for decomposition.
+    initial_components: array
+        Initial starting conditions of intensity components. Assumed to be shape (n_components, n_data).
+        If q_range is given, these will be trimmed in accordance with I.
+    fix_components: tuple(bool)
+        Flags for fixing a subset of initial components
+    mode: {"Linear", "Deconvolutional"}
+        Operating mode
+    kernel_width: int
+        Width of 1-dimensional convolutional kernel
+    max_iter: int
+        Maximum number of iterations for NMF
+    bkg_removal: int, optional
+        Integer degree for peakutils background removal
+    normalize: bool, optional
+        Flag for min-max normalization
+    **kwargs: dict
+        Arguments passed to the fit method. See nmf.models.NMFBase.
+
+    Returns
+    -------
+    sub_Q : array
+        Subsampled ordinate used for NMF
+    sub_I : array
+        Subsampled I used for NMF
+    alphas : array
+        Resultant weights from NMF
+    components:  array
+        Resultant components from NMF
+
+    """
+
+    # Data subselection
+    if q_range is None:
+        idx_min = 0
+        idx_max = I.shape[1]
+    else:
+        idx_min = np.where(Q[0, :] < q_range[0])[0][-1] if len(np.where(Q[0, :] < q_range[0])[0]) else 0
+        idx_max = np.where(Q[0, :] > q_range[1])[0][0] if len(np.where(Q[0, :] > q_range[1])[0]) else I.shape[1]
+
+    sub_I = I[:, idx_min:idx_max]
+    sub_Q = Q[:, idx_min:idx_max]
+
+    # Data manipulation
+    if bkg_removal:
+        import peakutils
+        bases = []
+        for i in range(sub_I.shape[0]):
+            bases.append(peakutils.baseline(sub_I[i, :], deg=bkg_removal))
+        bases = np.stack(bases)
+        sub_I = sub_I - bases
+    if normalize:
+        sub_I = (sub_I - np.min(I, axis=1, keepdims=True)) / (
+                np.max(sub_I, axis=1, keepdims=True) - np.min(sub_I, axis=1, keepdims=True))
+
+    # Numerical stability of non-negativity
+    if np.min(sub_I) < 0:
+        sub_I = sub_I - np.min(sub_I, axis=1, keepdims=True)
+
+    # Initial components
+    n_features = sub_I.shape[1]
+    if initial_components is None:
+        input_H = None
+    else:
+        input_H = []
+        for i in range(n_components):
+            try:
+                input_H.append(torch.tensor(initial_components[i][idx_min:idx_max],
+                                            dtype=torch.float).reshape(1, n_features))
+            except IndexError:
+                input_H.append(torch.rand(1, n_features))
+
+    # Model construction
+    if mode == "Linear":
+        model = NMF(sub_I.shape, n_components, initial_components=input_H, fix_components=fix_components)
+    elif mode == "Deconvolutional":
+        model = NMFD(sub_I.shape, n_components, T=kernel_width, initial_components=input_H,
+                     fix_components=fix_components)
+    else:
+        raise NotImplementedError
+
+    _, W = model.fit_transform(max_iter=max_iter, **kwargs)
+
+    if len(W.shape) > 2:
+        alphas = torch.mean(W, 2).data.numpy()
+    else:
+        alphas = W.data.numpy()
+
+    components = torch.cat([x for x in model.H_list]).data.numpy()
+    return sub_Q, sub_I, alphas, components
+
+
+def waterfall(ax, xs, ys, alphas, color='k', sampling=1, offset=0.2, **kwargs):
+    indicies = range(0, xs.shape[0])[::sampling]
+    for plt_i, idx in enumerate(indicies):
+        y = ys[idx, :] + plt_i * offset
+        x = xs[idx, :]
+        ax.plot(x, y, color=color, alpha=alphas[idx], **kwargs)
+    return ax
+
+
+def example_plot(sub_Q, sub_I, alphas, axes=None, sax=None, components=None, comax=None, cmap='tab10',
+                 alt_ordinate=None, offset=1.,
+                 summary_fig=False):
+    """
+    Example plotting of NMF results. Not necessarily for Bluesky deployment
+
+    Parameters
+    ----------
+    sub_Q: array
+        Q to plot in I(Q)
+    sub_I: array
+        I to plot in I(Q)
+    alphas: array
+        transparencies of multiple repeated plots of I(Q)
+    axes: optional existing axes for waterfalls
+    sax: optional axes for summary figure
+    cmap: mpl colormap
+    alt_ordinate: array
+        Array len sub_I.shape[0], corresponding to an alternative labeled dimension for which to order the stacked plots
+    summary_fig: bool
+        Whether to include separate figure of alphas over the ordinate
+
+    Returns
+    -------
+    fig, axes
+
+    """
+
+    n_components = alphas.shape[1]
+    cmap = mpl.cm.get_cmap(cmap)
+    norm = mpl.colors.Normalize(vmin=0, vmax=n_components)
+
+    # Create alternative ordinate for the waterfall/stacking
+    if not alt_ordinate is None:
+        idxs, labels = list(zip(*sorted(zip(range(sub_I.shape[0]), alt_ordinate), key=lambda x: x[1])))
+    else:
+        idxs = list(range(sub_I.shape[0]))
+        labels = list(range(sub_I.shape[0]))
+    xs = sub_Q[idxs, :]
+    ys = sub_I[idxs, :]
+    alphas = alphas[idxs, :]
+
+    # Order by proxy center of mass of class in plot regime. Makes the plots feel like a progression not random.
+    alpha_ord = np.argsort(np.matmul(np.arange(alphas.shape[0]), alphas))
+
+    if axes is None:
+        fig, axes = plt.subplots(int(np.ceil(np.sqrt(n_components))), int(np.ceil(np.sqrt(n_components))))
+        axes = axes.reshape(-1)
+    else:
+        axes = np.ravel(axes)
+    for i, ax in enumerate(axes):
+        if i < n_components:
+            i_a = alpha_ord[i]
+            color = cmap(norm(i))
+            alpha = (alphas[:, i_a] - np.min(alphas[:, i_a])) / (np.max(alphas[:, i_a]) - np.min(alphas[:, i_a]))
+            ax = waterfall(ax, xs, ys, alpha, color=color, offset=offset)
+        else:
+            ax.set_visible = False
+
+    if summary_fig:
+        if sax is None:
+            sfig, sax = plt.subplots(figsize=(6, 6))
+
+        sx = np.arange(0, alphas.shape[0])
+        for i in range(alphas.shape[1]):
+            sax.plot(sx, alphas[:, alpha_ord[i]], color=cmap(norm(i)), label=f"Component {i + 1}")
+
+    if components is not None:
+        if comax is None:
+            comfig, comax = plt.subplots(figsize=(6, 6))
+        for i in range(components.shape[0]):
+            comax.plot(xs[0, :], components[i, :]+i, color=cmap(norm(i)))
+
+    return
