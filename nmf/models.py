05903dcda962738ef21e1a952a8ebf380897f95b
maffettone <phillip.maffettone@gmail.com>
2021-02-12 12:45:45 -0500

    Working NMF and NMFD with weight constraints.

--- nmf/models.py
+++ nmf/models.py
@@ -7,7 +7,9 @@ from nmf.metrics import Beta_divergence
 def _mu_update(param, pos, gamma, l1_reg, l2_reg):
     if isinstance(param, nn.ParameterList):
         # Handle no gradients in fixed components
-        grad = torch.cat([x.grad if x.requires_grad else torch.zeros_like(x) for x in param])
+        grad = torch.cat(
+            [x.grad if x.requires_grad else torch.zeros_like(x) for x in param]
+        )
     elif param.grad is None:
         return
     else:
@@ -38,13 +40,19 @@ def _mu_update(param, pos, gamma, l1_reg, l2_reg):
 
 
 class NMFBase(nn.Module):
-    def __init__(self, W_shape, H_shape,
-                 n_components,
-                 initial_components=None,
-                 fix_components=()):
+    def __init__(
+            self,
+            W_shape,
+            H_shape,
+            n_components,
+            initial_components=None,
+            fix_components=(),
+            initial_weights=None,
+            fix_weights=(),
+    ):
         """
         Base class for setting up NMF
-        W is (m_examples, n_components)
+        W is (m_examples, n_components) or (m_examples, n_components, kernel_width)
         H is (n_components, n_example_features)
         W @ H give reconstruction of X.
         Parameters
@@ -56,28 +64,42 @@ class NMFBase(nn.Module):
         fix_components
         """
         super().__init__()
-        self.fix_neg = nn.Threshold(0., 1e-8)
+        self.fix_neg = nn.Threshold(0.0, 1e-8)
         self.rank = n_components
 
         W = torch.rand(*W_shape)
         self.W = nn.Parameter(W)
-        if initial_components is not None:
-            l = [nn.Parameter(component) for component in initial_components]
+        if initial_weights is not None:
+            w_list = [nn.Parameter(weight) for weight in initial_weights]
         else:
-            l = [nn.Parameter(torch.rand(1, *H_shape[1:])) for _ in range(H_shape[0])]
+            w_list = [nn.Parameter(torch.rand(1, *W_shape[1:])) for _ in range(W_shape[0])]
+        if fix_weights:
+            for i in range(len(fix_weights)):
+                w_list[i].requires_grad = not fix_weights[i]
+        self.W_list = nn.ParameterList(w_list)
+
+        if initial_components is not None:
+            h_list = [nn.Parameter(component) for component in initial_components]
+        else:
+            h_list = [nn.Parameter(torch.rand(1, *H_shape[1:])) for _ in range(H_shape[0])]
         if fix_components:
             for i in range(len(fix_components)):
-                l[i].requires_grad = not fix_components[i]
-        self.H_list = nn.ParameterList()
-        for param in l:
-            self.H_list.append(param)
+                h_list[i].requires_grad = not fix_components[i]
+        self.H_list = nn.ParameterList(h_list)
 
-    def forward(self, H_list=None, W=None):
-        if H_list is None:
-            H_list = self.H_list
+    @property
+    def H(self):
+        return torch.cat([x for x in self.H_list])
+
+    @property
+    def W(self):
+        return torch.cat([x for x in self.W_list])
+
+    def forward(self, H=None, W=None):
+        if H is None:
+            H = self.H
         if W is None:
             W = self.W
-        H = torch.cat([x for x in H_list])
         return self.reconstruct(H, W)
 
     def reconstruct(self, H, W):
@@ -89,7 +111,8 @@ class NMFBase(nn.Module):
     def get_H_positive(self, WH, beta, W_sum) -> (torch.Tensor, None or torch.Tensor):
         raise NotImplementedError
 
-    def fit(self,
+    def fit(
+            self,
             X,
             update_W=True,
             update_H=True,
@@ -97,7 +120,8 @@ class NMFBase(nn.Module):
             tol=1e-5,
             max_iter=200,
             alpha=0,
-            l1_ratio=0):
+            l1_ratio=0,
+    ):
 
         X = X.type(torch.float)
         X = self.fix_neg(X)
@@ -119,21 +143,22 @@ class NMFBase(nn.Module):
         H_sum, W_sum = None, None
 
         for n_iter in range(max_iter):
-            H = torch.cat([x for x in self.H_list])
-            if self.W.requires_grad:
+            # W update
+            if update_W and any([x.requires_grad for x in self.W_list]):
                 self.zero_grad()
-                WH = self.reconstruct(H.detach(), self.W)
+                WH = self.reconstruct(self.H.detach(), self.W)
                 loss = Beta_divergence(self.fix_neg(WH), X, beta)
                 loss.backward()
 
                 with torch.no_grad():
                     positive_comps, H_sum = self.get_W_positive(WH, beta, H_sum)
-                    _mu_update(self.W, positive_comps, gamma, l1_reg, l2_reg)
+                    _mu_update(self.W_list, positive_comps, gamma, l1_reg, l2_reg)
                 W_sum = None
 
-            if any([x.requires_grad for x in self.H_list]):
+            # H update
+            if update_H and any([x.requires_grad for x in self.H_list]):
                 self.zero_grad()
-                WH = self.reconstruct(H, self.W.detach())
+                WH = self.reconstruct(self.H, self.W.detach())
                 loss = Beta_divergence(self.fix_neg(WH), X, beta)
                 loss.backward()
 
@@ -154,20 +179,24 @@ class NMFBase(nn.Module):
 
     def fit_transform(self, *args, **kwargs):
         n_iter = self.fit(*args, **kwargs)
-        return n_iter, self.W
+        return n_iter, torch.cat([x for x in self.W_list])
 
 
 class NMF(NMFBase):
-
     def __init__(self, X_shape, n_components, **kwargs):
         self.m_examples, self.n_features = X_shape
-        super().__init__((self.m_examples, n_components), (n_components, self.n_features), n_components, **kwargs)
+        super().__init__(
+            (self.m_examples, n_components),
+            (n_components, self.n_features),
+            n_components,
+            **kwargs
+        )
 
     def reconstruct(self, H, W):
         return W @ H
 
     def get_W_positive(self, WH, beta, H_sum):
-        H = torch.cat([x for x in self.H_list])
+        H = self.H
         if beta == 1:
             if H_sum is None:
                 H_sum = H.sum(1)
@@ -194,24 +223,25 @@ class NMF(NMFBase):
         return denominator, W_sum
 
     def sort(self):
-        _, maxidx = self.W.data.max(0)
-        _, idx = maxidx.sort()
-        self.W.data = self.W.data[:, idx]
-        for H in self.H_list:
-            H.data = H.data[idx]
+        raise NotImplementedError
+
 
 class NMFD(NMFBase):
-
     def __init__(self, X_shape, n_components, T=1, **kwargs):
         self.m_examples, self.n_features = X_shape
-        self.pad_size = T-1
-        super().__init__((self.m_examples, n_components, T), (n_components, self.n_features - T + 1), n_components, **kwargs)
+        self.pad_size = T - 1
+        super().__init__(
+            (self.m_examples, n_components, T),
+            (n_components, self.n_features - T + 1),
+            n_components,
+            **kwargs
+        )
 
     def reconstruct(self, H, W):
         return F.conv1d(H[None, :], W.flip(2), padding=self.pad_size)[0]
 
     def get_W_positive(self, WH, beta, H_sum):
-        H = torch.cat([x for x in self.H_list])
+        H = self.H
         if beta == 1:
             if H_sum is None:
                 H_sum = H.sum(1)
@@ -238,11 +268,8 @@ class NMFD(NMFBase):
         return denominator, W_sum
 
     def sort(self):
-        _, maxidx = self.W.data.sum(2).max(0)
-        _, idx = maxidx.sort()
-        self.W.data = self.W.data[:, idx]
-        for H in self.H_list:
-            H.data = H.data[idx]
+        raise NotImplementedError
+
 
 if __name__ == "__main__":
     from nmf.models import NMF
@@ -251,20 +278,24 @@ if __name__ == "__main__":
 
 
     def gaussian(x, mu, sig):
-        return np.exp(-np.power(x - mu, 2.) / (2 * np.power(sig, 2.)))
+        return np.exp(-np.power(x - mu, 2.0) / (2 * np.power(sig, 2.0)))
 
 
     n_features = 1000
     m_patterns = 10
     x = np.linspace(-10, 10, n_features)
-    y1 = gaussian(x, 0, 1) + np.random.normal(0, .01, (m_patterns, n_features))
-    y2 = gaussian(x, -5, 1) + np.random.normal(0, .01, (m_patterns, n_features))
-    y3 = gaussian(x, 5, 1) + np.random.normal(0, .01, (m_patterns, n_features))
+    y1 = gaussian(x, 0, 1) + np.random.normal(0, 0.01, (m_patterns, n_features))
+    y2 = gaussian(x, -5, 1) + np.random.normal(0, 0.01, (m_patterns, n_features))
+    y3 = gaussian(x, 5, 1) + np.random.normal(0, 0.01, (m_patterns, n_features))
     Y = np.concatenate((y1, y2, y3), axis=0)
     fixin = gaussian(x, 0.1, 1)
-    input_H = (torch.tensor(fixin, dtype=torch.float).reshape(1, n_features),
-               torch.rand(1, n_features),
-               torch.rand(1, n_features))
-    model = NMFD(Y.shape, 3, initial_components=input_H, T=1, fix_components=(True, False, False))
-    #model = NMFD(Y.shape,3, T=5)
+    input_H = (
+        torch.tensor(fixin, dtype=torch.float).reshape(1, n_features),
+        torch.rand(1, n_features),
+        torch.rand(1, n_features),
+    )
+    model = NMFD(
+        Y.shape, 3, initial_components=input_H, T=1, fix_components=(True, False, False)
+    )
+    # model = NMFD(Y.shape,3, T=5)
     n_iter = model.fit(torch.tensor(Y), beta=2, alpha=0.5)
